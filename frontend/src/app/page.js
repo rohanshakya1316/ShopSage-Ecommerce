8480b2bcae8a2e8fbb08f4720db0ca5ae920912e
rohanshakya1316 <romanrohanreignsshakya@gmail.com>
2026-07-06 21:57:47 +0545

    Feat: Dynamic UI programming setup completed in next.js

---
+++ frontend/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>Hello World from Rohan Shakya.
+      <h1>For Dynamic UI file configuration setup complete.</h1>
+    </div>
+  );
+}
