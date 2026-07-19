e6482099e42160168411373ed1dba0c6ce49fb97
rohanshakya1316 <romanrohanreignsshakya@gmail.com>
2026-07-19 19:58:29 +0545

    config: Image Configuration for unsplash and picsum

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
         port: "",
         pathname: "/daijnijpm/image/**",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos"
+      }
     ],
   },
 };
